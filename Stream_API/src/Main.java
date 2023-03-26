e456a4610a10c6d1d2a212c5d1bfe60e71cd017a
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-03-26 19:09:20 +0530

    Stram API Updated

--- Stream_API/src/Main.java
+++ Stream_API/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,37 @@ public class Main {
         System.out.println(sqList);
 
         list1.stream().map(i->i*i).forEach(System.out::println);
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "New York"));
+        students.add(new Student(2, "Los Angeles"));
+        students.add(new Student(3, "Chicago"));
+        students.add(new Student(4, "Houston"));
+        students.add(new Student(5, "New York"));
+
+        List<Student> filteredStudents = students.stream()
+                .filter(s -> s.getId() == 1 && s.getCity().equals("New York"))
+                .collect(Collectors.toList());
+
+        filteredStudents.forEach(s -> System.out.println("ID: " + s.getId() + ", City: " + s.getCity()));
+
+        List<Student> filteredStudentsMap = students.stream()
+                .map(s -> {
+                    if (s.getId() == 1 && s.getCity().equals("New York")) {
+                        return s;
+                    } else {
+                        return null;
+                    }
+                })
+                .filter(s -> {
+                    if (s == null) {
+                        return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toList());
+
+        filteredStudentsMap.forEach(s -> System.out.println("ID: " + s.getId() + ", City: " + s.getCity()));
 
     }
 }
---
+++ Stream_API/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private int id;
+    private String city;
+
+    public Student(int id, String city) {
+        this.id = id;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
